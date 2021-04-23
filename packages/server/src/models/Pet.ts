636fe479b0151af0bb53443ade58dcd0d625aeac
Richard Zilahi <richard.zilahi@salesframe.com>
2021-04-24 00:14:15 +0300

    implementing Pet modal

--- packages/server/src/models/Pet.ts
+++ packages/server/src/models/Pet.ts
@@ -11,6 +11,37 @@ export interface IPet extends Document {
 	name: string
 	photoUrls: Array<string>
 	status: Status
-	user: ICategory['_id']
+	category: ICategory['_id']
 	tags: Array<ICategory['_id']>
 }
+
+const petSchema: Schema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	photoUrls: [
+		{
+			type: String,
+		},
+	],
+	status: {
+		type: Boolean,
+		required: true,
+	},
+	category: {
+		type: Schema.Types.ObjectId,
+		ref: 'Category',
+	},
+	tags: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Tags',
+		},
+	],
+})
+
+const Pet: Model<IPet> = model('Pet', petSchema)
+
+export default Pet
--- packages/server/src/models/Profile.ts
+++ packages/server/src/models/Profile.ts
@@ -1,5 +1,5 @@
-import { Document, Model, model, Schema } from "mongoose";
-import { IUser } from "./User";
+import { Document, Model, model, Schema } from 'mongoose'
+import { IUser } from './User'
 
 /**
  * Interface to model the Profile Schema for TypeScript.
@@ -9,36 +9,36 @@ import { IUser } from "./User";
  * @param username:string
  */
 export interface IProfile extends Document {
-  user: IUser["_id"];
-  firstName: string;
-  lastName: string;
-  username: string;
+	user: IUser['_id']
+	firstName: string
+	lastName: string
+	username: string
 }
 
 const profileSchema: Schema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
-  },
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-});
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+	},
+	firstName: {
+		type: String,
+		required: true,
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+})
 
-const Profile: Model<IProfile> = model("Profile", profileSchema);
+const Profile: Model<IProfile> = model('Profile', profileSchema)
 
-export default Profile;
+export default Profile

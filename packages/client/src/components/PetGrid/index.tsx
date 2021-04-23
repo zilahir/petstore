2f6d3a86a5ac1f15ee5d57ed1990d1003b0a7a7c
Richard Zilahi <richard.zilahi@salesframe.com>
2021-04-23 23:51:37 +0300

    implementing petGrid

---
+++ packages/client/src/components/PetGrid/index.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react'
+
+import styles from './PetGrid.module.scss'
+
+const PetGrid = (): ReactElement => <div className={styles.gridRoot}>helo</div>
+
+export default PetGrid
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { QueryClientProvider } from 'react-query'
 import ReactDOM from 'react-dom'
+
 import App from './app'
+import queryClient from './api/queryClient'
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Router>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
 		</Router>
 	</React.StrictMode>,
 	document.querySelector('#root'),

01b97b22b0ffd3bb9180a280a755d3b9dec6bbdb
Richard Zilahi <richard.zilahi@salesframe.com>
2021-04-25 13:34:40 +0300

    added all fields to register page

--- packages/client/src/pages/Register/index.tsx
+++ packages/client/src/pages/Register/index.tsx
@@ -7,10 +7,13 @@ import Input from '../../components/common/Input'
 import Button from '../../components/common/Button'
 
 const Register = (): ReactElement => {
-	const [userName, setUserName] = useState('')
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 	const [passwordConfirm, setPasswordConfirm] = useState('')
+	const [userName, setUserName] = useState('')
+	const [firstName, setFirstName] = useState('')
+	const [lastName, setLastName] = useState('')
+	const [phone, setPhone] = useState('')
 	return (
 		<Layout hasHeader={false}>
 			<div className={styles.rootContainer}>
@@ -19,25 +22,47 @@ const Register = (): ReactElement => {
 					<div className={styles.loginInner}>
 						<Input
 							label="Username"
-							placeHolder="Username"
+							placeHolder="dolphin99"
 							onChange={event => setUserName(event.target.value)}
 							value={userName}
 						/>
 						<Input
 							label="Email"
-							placeHolder="Email"
+							placeHolder="demo@demo.fi"
 							onChange={event => setEmail(event.target.value)}
 							value={email}
 						/>
+						<div className={styles.group}>
+							<Input
+								label="Password"
+								onChange={event => setPassword(event.target.value)}
+								value={password}
+							/>
+							<Input
+								label="Confirm Password"
+								onChange={event => setPasswordConfirm(event.target.value)}
+								value={passwordConfirm}
+							/>
+						</div>
+						<div className={styles.group}>
+							<Input
+								label="Firstname"
+								placeHolder="John"
+								onChange={event => setFirstName(event.target.value)}
+								value={firstName}
+							/>
+							<Input
+								label="Lastname"
+								placeHolder="Smith"
+								onChange={event => setLastName(event.target.value)}
+								value={lastName}
+							/>
+						</div>
 						<Input
-							label="Password"
-							onChange={event => setPassword(event.target.value)}
-							value={password}
-						/>
-						<Input
-							label="Confirm Password"
-							onChange={event => setPasswordConfirm(event.target.value)}
-							value={passwordConfirm}
+							label="Phone"
+							placeHolder="0406678965"
+							onChange={event => setPhone(event.target.value)}
+							value={phone}
 						/>
 						<div className={styles.btnContainer}>
 							<Button label="Sign up" onClick={() => alert('pressed')} />

15bdb7bb5d4e19beee377993144d76a605279715
Richard Zilahi <richard.zilahi@salesframe.com>
2021-04-26 13:36:05 +0300

    added test for delete

--- packages/server/tests/server.test.ts
+++ packages/server/tests/server.test.ts
@@ -95,3 +95,36 @@ test('/POST /api/pet', async done => {
 		done()
 	})
 })
+
+test('/DELETE /api/pet', async done => {
+	const tags = await Tag.find({})
+	const categories = await Category.find({})
+	const numOfTag = random.int(1, tags.length)
+	const numOfCategory = random.int(1, categories.length)
+	const tagsForThisPet = tags.slice(0, numOfTag)
+	const categoiesForThisPet = categories[numOfCategory]
+	const randomDogImage = faker.image.animals(500, 500)
+	const newUser: UserInterface = {
+		username: faker.internet.userName(),
+		firstName: faker.name.firstName(0),
+		lastName: faker.name.lastName(1),
+		email: faker.internet.email(),
+		password: 'demo123',
+		phone: '0406753038',
+		userStatus: 'active',
+	}
+
+	const user = await User.create(newUser)
+	const newPet: PetInterface = {
+		userId: user.id,
+		name: faker.lorem.word(5),
+		status: Status.available,
+		category: categoiesForThisPet,
+		tags: tagsForThisPet,
+		photoUrls: [randomDogImage],
+	}
+	const pet = await Pet.create(newPet)
+	Pet.findOneAndDelete({ id: pet.id }).then(() => {
+		done()
+	})
+})
